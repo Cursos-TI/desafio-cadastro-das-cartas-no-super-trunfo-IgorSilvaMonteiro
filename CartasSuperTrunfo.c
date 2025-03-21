3d2af2b208bad37c0567fdce52f42a158468331c
IgorSilvaMonteiro <164340735+IgorSilvaMonteiro@users.noreply.github.com>
2025-03-21 19:07:17 +0000

    nivel 2 do desafio

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,21 +1,25 @@
 #include <stdio.h>
 
 int main() {   
-
+    
+    // variaveis
     char estado1[10], estado2[10];
-    int população1, população2;
+    int populacao1,populacao2;
     float area1, area2;
     float bip1, bip2;
     int ponto_turistico1, ponto_turistico2;
+    float densidade_populaciona, densidade_populaciona2;
+    float per_capita, per_capita2;
 
-    printf("Desafio Super Trunfo - Países");
+    printf("Desafio Super Trunfo - Países\n");
+    printf("CARTA 1\n");
 
     // carta 1 - entrada
     printf ("Digite a sigla do estado:");
     scanf ("%s", &estado1);
 
     printf ("população do estado:");
-    scanf ("%d", &população1);
+    scanf ("%d", &populacao1);
 
     printf ("area do estado:");
     scanf ("%f", &area1);
@@ -27,11 +31,13 @@ int main() {
     scanf ("%d", &ponto_turistico1);
 
     // carta 2 - entrada
+    printf("CARTA 2\n");
+
     printf ("Digite a sigla do estado:");
     scanf ("%s", &estado2);
 
     printf ("população do estado:");
-    scanf ("%d", &população2);
+    scanf ("%d", &populacao2);
 
     printf ("area do estado:");
     scanf ("%f", &area2);
@@ -43,20 +49,33 @@ int main() {
     scanf ("%d", &ponto_turistico2);
 
     //carta 1 - saida
-    printf ("Carta1\n");
+    printf ("Saida - Carta1\n");
     printf ("Estado: %s\n", estado1);
-    printf ("população: %d\n", população1);
-    printf ("area: %f\n", area1);
-    printf ("bip: %f\n", bip1);
-    printf ("ponto turistico: %s\n", ponto_turistico1);
+    printf ("população: %d\n", populacao1);
+    printf ("area: %.2f\n", area1);
+    printf ("bip: %.2f\n", bip1);
+    printf ("ponto turistico: %d\n", ponto_turistico1);
 
      //carta 2 - saida
-     printf ("Carta2\n");
+     printf ("Saida - Carta2\n");
      printf ("Estado: %s\n", estado2);
-     printf ("população: %d\n", população2);
-     printf ("area: %f\n", area2);
-     printf ("bip: %f\n", bip2);
-     printf ("ponto turistico: %s\n", ponto_turistico2);
+     printf ("população: %d\n", populacao2);
+     printf ("area: %.2f\n", area2);
+     printf ("bip: %.2f\n", bip2);
+     printf ("ponto turistico: %d\n", ponto_turistico2);
+
+    //nivel 2 do desafio
+    printf("Medias - Carta 1\n");
+    densidade_populaciona = (float) (populacao1 / area1);
+    printf ("A densidade populacional é: %.2d\n", densidade_populaciona);
+    per_capita = (float) (bip1 / populacao1);
+    printf ("A PIB per capita é: %.2d\n", per_capita);
+
+    printf("Medias - Carta 2\n");
+    densidade_populaciona2 = (float) (populacao2 / area2);
+    printf ("A densidade populacional é: %.2d\n", densidade_populaciona);
+    per_capita2 = (float) (bip2 / populacao2);
+    printf ("A PIB per capita é: %.2d\n", per_capita);
 
      return 0;
      
